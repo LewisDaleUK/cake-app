c5e5730c426e675cc86fe18beae4afc5b2b66a8b
Lewis Dale <lewis.dale@fortacode.co.uk>
2018-07-02 15:53:18 +0100

    Added CSS and material ui for a basic user interface

--- src/components/CakeView.js
+++ src/components/CakeView.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CloseIcon from '@material-ui/icons/Close';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import '../css/CakeView.css';
 
 const CakeView = ({ cake }) => (
   <div className="cake-view">
-    <Link to="/">
-    <div
-      className="button"
-      title="close"
-    >
-      X
-    </div>
-  </Link>
-    <h2>{ cake.name }</h2>
-    <img src={cake.imageUrl} alt={cake.name} />
-    <p className="comment">
-      { cake.comment }
-    </p>
-    <p className="yumfactor">{ cake.yumFactor }</p>
+    <Card>
+      <Link to="/">
+        <CloseIcon style={{ width: 40, height: 40 }}/>
+      </Link>
+      <img src={cake.imageUrl} alt={cake.name} />
+      <CardContent>
+        <h2>{ cake.name }</h2>
+        <p className="comment">
+          { cake.comment }
+        </p>
+        <p className="yumfactor">Yum Factor: { cake.yumFactor }</p>
+      </CardContent>
+    </Card>
   </div>
 );
 
