6d3d80e730d0d3f9d29d562eacdbb453e333b74b
Lewis Dale <lewis.dale@fortacode.co.uk>
2018-07-02 11:25:59 +0100

    Created basic components for cake list

---
+++ src/components/CakeList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CakeList = ({ cakes }) => (
+  <div className="cakelist">
+    <h2>Cake List</h2>
+  </div>
+);
+
+export default CakeList;
---
+++ src/containers/CakeListContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import CakeList from '../components/CakeList';
+
+export default class CakeListContainer extends Component {
+  render() {
+    return (<CakeList />);
+  }
+}
