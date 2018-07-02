c533c8deaf57a5bdfb5b2ac146b1ea3fdd10d7f2
Lewis Dale <lewis.dale@fortacode.co.uk>
2018-07-02 11:56:15 +0100

    Created route and view for creating a cake

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CakeListContainer from './containers/CakeListContainer';
+import CreateCakeContainer from './containers/CreateCakeContainer';
 import './App.css';
 
 class App extends Component {
@@ -12,7 +13,8 @@ class App extends Component {
             <h1 className="app-title">Cake App</h1>
           </header>
           <div className="content">
-            <Route exact path="/" component={() => (<CakeListContainer />)} />
+            <Route exact path="/" component={CakeListContainer} />
+            <Route path="/cake/create" component={CreateCakeContainer} />
           </div>
         </div>
       </Router>
---
+++ src/containers/CreateCakeContainer.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class CreateCakeContainer extends Component {
+  render() {
+    return (<h1>Create Cake</h1>);
+  }
+};
