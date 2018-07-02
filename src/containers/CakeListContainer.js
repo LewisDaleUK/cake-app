347c6009e7467fa8a44557e2bc29345fd802d405
Lewis Dale <lewis.dale@fortacode.co.uk>
2018-07-02 11:26:20 +0100

    Added short description for cakelistcontainer

--- src/containers/CakeListContainer.js
+++ src/containers/CakeListContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import CakeList from '../components/CakeList';
 
+// Container component for the Cake List.
+// Manages cake list state
 export default class CakeListContainer extends Component {
   render() {
     return (<CakeList />);
