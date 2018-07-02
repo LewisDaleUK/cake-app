7f4262d2fffd24b953a296fba1855492ee7adda0
Lewis Dale <lewis.dale@fortacode.co.uk>
2018-07-02 11:34:22 +0100

    Added short list of cakes to constants

--- src/containers/CakeListContainer.js
+++ src/containers/CakeListContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import CakeList from '../components/CakeList';
+import cakes from '../constants/cakes';
 
 // Container component for the Cake List.
 // Manages cake list state
 export default class CakeListContainer extends Component {
   render() {
-    return (<CakeList />);
+    return (<CakeList cakes={cakes} />);
   }
 }
